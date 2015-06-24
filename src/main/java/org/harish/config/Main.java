0891eff56613fe8397b63c2e8b04f1abaaceda80
harish-sharma <harish.sharma@inmobi.com>
2015-06-25 00:47:54 +0530

    Adding main class for printing some sample values from resources.

---
+++ src/main/java/org/harish/config/Main.java
@@ -0,0 +1,17 @@
+package org.harish.config;
+
+
+public class Main {
+    public static void main(String[] args) throws LoadException {
+        ConfigLoader loader = new DefaultConfigLoader();
+        Config cfg = loader.loadConfig("src/main/resources/configfile1", "ubuntu", "production");
+        System.out.println(cfg.getConfig("common.paid_users_size_limit"));
+        System.out.println(cfg.getConfig("ftp.name"));
+        System.out.println(cfg.getConfig("http.params"));
+        System.out.println(cfg.getConfig("ftp.lastname"));
+        System.out.println(cfg.getConfig("ftp.enabled"));
+        System.out.println(cfg.getConfig("ftp.path"));
+        System.out.println(cfg.getConfig("ftp"));
+        System.out.println(cfg.getConfig("http"));
+    }
+}
