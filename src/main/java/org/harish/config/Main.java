c3e23a653c4d3254076b98265bc5882e0d81bf94
harish-sharma <harish.sharma@inmobi.com>
2015-06-25 01:44:13 +0530

    Some changes.

--- src/main/java/org/harish/config/Main.java
+++ src/main/java/org/harish/config/Main.java
@@ -9,19 +9,19 @@ import java.io.Reader;
  * @author harish.sharma
  */
 public class Main {
+
     public static void main(String[] args) throws LoadException, IOException {
         ConfigLoader loader = new DefaultConfigLoader();
         InputStream in = Main.class.getClassLoader().getResourceAsStream("configfile1");
         Reader reader = new InputStreamReader(in);
         Config cfg = loader.loadConfig(reader, "ubuntu", "production");
-        System.out.println(cfg.getConfig("common.paid_users_size_limit"));
+        System.out.println(cfg.getConfigAsLong("common.paid_users_size_limit"));
         System.out.println(cfg.getConfig("ftp.name"));
         System.out.println(cfg.getConfig("http.params"));
         System.out.println(cfg.getConfig("ftp.lastname"));
-        System.out.println(cfg.getConfig("ftp.enabled"));
+        System.out.println(cfg.getConfigAsBoolean("ftp.enabled"));
         System.out.println(cfg.getConfig("ftp.path"));
         System.out.println(cfg.getConfig("ftp"));
-        System.out.println(cfg.getConfig("http"));
         in.close();
     }
 }
